8f6efdd8f3f7c1f3b9ae3f8d49ed8d8b4d944212
Maurício <mauriciopekka123@gmail.com>
2025-09-16 16:41:57 -0300

    .

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -22,16 +22,16 @@ export default function Footer() {
         <div className="w-60 md:w-120 h-0.5 bg-black"></div>
       </div>
 
-      <div className="w-full flex flex-col items-center px-4 py-8 md:py-12 gap-8">
-        <div className="flex flex-col items-center gap-4">
+      <div className="w-full flex flex-col md:flex-row px-4 md:px-70 py-8 md:py-12 gap-30">
+        <div className="flex flex-col gap-4 md:mt-4">
           <img src="/images/Logo.svg" alt="Logo" className="w-40 md:w-60" />
-          <div className="text-sm md:text-base text-center">Rua Henrique Novaes, Centro,<br /> Itaguaçu/ES</div>
+          <div className="text-sm md:text-base">Rua Henrique Novaes, Centro,<br /> Itaguaçu/ES</div>
           <h1 className="text-2xl md:text-4xl">0800 591 8681</h1>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-8 md:gap-16 text-center">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-16">
           <div className="flex flex-col gap-4">
-            <div className="text-xl md:text-2xl">Menu</div>
+            <div className="text-xl md:text-xl font-bold">Menu</div>
             <div>
               <ul className="flex flex-col gap-2">
                 {menu.map((item) => (
@@ -44,7 +44,7 @@ export default function Footer() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <div className="text-xl md:text-2xl">Serviços</div>
+            <div className="text-xl font-bold">Serviços</div>
             <div>
               <ul className="flex flex-col gap-2">
                 {servicos.map((item) => (
@@ -57,18 +57,18 @@ export default function Footer() {
           </div>
 
           <div className="flex flex-col gap-4">
-            <div className="text-xl md:text-2xl">Contato</div>
+            <div className="text-xl font-bold">Contato</div>
             <div className="flex flex-col gap-2">
               <div className="text-sm md:text-base"> 
                 Whatsapp: <br /> 0800 591 8681
               </div>
 
               <div className="text-sm md:text-base">
-                E-mail: <br /> contato@massa.com.br
+                E-mail: <br /> contato@massa.net.br
               </div>
 
               <div className="leading-tight text-sm md:text-base">
-                Horário de atendimento<br />De seg-sex entre 08:00 à<br />18:00 e Sab
+                Horário de atendimento<br />De seg-sex entre 08:00h à<br />18:00h e Sab entre 08:00h à<br />17:00h
               </div>
             </div>
           </div>
@@ -77,9 +77,9 @@ export default function Footer() {
 
       <div className="w-9/10 h-0.5 bg-black mx-auto"></div>
 
-      <div className="w-full flex flex-col items-center px-4 py-4 gap-4">
-        <div className="text-sm md:text-base text-center">© 2025 Massa Internet -Todos os direitos reservados</div>
-        <div className="text-sm md:text-base text-center">Termos de serviço   |   Políticas de Privacidade</div>
+      <div className="w-full flex flex-row justify-between px-4 md:px-60 py-4 gap-4">
+        <div className="text-sm md:text-base">© 2025 Massa Internet -Todos os direitos reservados</div>
+        <div className="text-sm md:text-base">Termos de serviço   |   Políticas de Privacidade</div>
       </div>
       
     </footer>
