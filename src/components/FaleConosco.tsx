c36153e5f6d1246007cfdbf1f9b7c248b45a1021
Maurício <mauriciopekka123@gmail.com>
2025-11-12 23:50:09 -0300

    .

--- src/components/FaleConosco.tsx
+++ src/components/FaleConosco.tsx
@@ -3,7 +3,7 @@ export default function FaleConosco() {
     <div className="w-full flex flex-col items-center justify-center bg-primary px-4 md:px-0">
       
       <h1 className="text-2xl md:text-4xl font-bold text-white mt-12 md:mt-24 text-center">Fale <span className="text-secondary">Conosco</span></h1>
-      <form className="w-full md:w-1/2 flex flex-col gap-4 mt-4 mb-12 md:mb-24">
+      <form className="w-full md:w-1/2 flex flex-col gap-4 mt-4 mb-12 md:mb-24 ">
         <div>
           <input
             id="nome"
